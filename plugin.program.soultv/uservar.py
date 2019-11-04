b40fa55ff9a4ae3098b895bb4cf185036a5d8ed7
Soul TV <SoulTVStudio@gmail.com>
2019-11-03 23:14:08 -0600

    edit

--- plugin.program.soultv/uservar.py
+++ plugin.program.soultv/uservar.py
@@ -15,7 +15,7 @@ ART = os.path.join(PATH, 'resources', 'art')
 #########################################################
 ADDONTITLE = '[B][COLOR grey]Soul TV[/COLOR][/B] Helper'
 BUILDERNAME = 'Seoul'
-EXCLUDES = [ADDON_ID, 'repository.soultv', 'pvr.iptvsimpleclient', 'inputstream.adaptive', 'inputstream.rtmp', 'service.coreelec.settings', 'service.libreelec.settings']
+EXCLUDES = [ADDON_ID, 'repository.soultv', 'service.coreelec.settings', 'service.libreelec.settings']
 # Text File with build info in it.
 BUILDFILE = 'https://raw.githubusercontent.com/SoulTV/soultv.github.io/master/Helper%20Files/text/builds.txt'
 # How often you would like it to check for build updates in days
